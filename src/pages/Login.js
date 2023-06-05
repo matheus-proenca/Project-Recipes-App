d7276c05a16202c95b398efbe30f4ed9f08f1b3f
Rodrigo Silva Ferreira <rodrigosferreira0@gmail.com>
2023-06-05 19:06:59 -0300

    requisito 4

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [atived, setAtived] = useState(true);
+
+  const minimo = 7;
+  const senhaCorreta = password.length >= minimo;
+  const emailCorreto = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(email);
+  const disabled = !(senhaCorreta && emailCorreto);
+
+  useEffect(() => {
+    setAtived(disabled);
+  }, [disabled]);
+
   return (
     <div className="login">
       <h1>Login</h1>
-      <input type="email" data-testid="email-input" />
-      <input type="password" data-testid="password-input" />
-      <button data-testid="login-submit-btn">Entrar</button>
+      <label htmlFor="email">
+        Email:
+        <input
+          type="email"
+          id="email"
+          data-testid="email-input"
+          placeholder="digite seu emal"
+          value={ email }
+          onChange={ (e) => setEmail(e.target.value) }
+        />
+      </label>
+      <label htmlFor="password">
+        Senha:
+        <input
+          type="password"
+          id="password"
+          data-testid="password-input"
+          placeholder="digite sua senha de 7 ou mais digitos"
+          value={ password }
+          onChange={ (e) => setPassword(e.target.value) }
+        />
+      </label>
+      <button data-testid="login-submit-btn" disabled={ atived }>Entrar</button>
     </div>
   );
 }
