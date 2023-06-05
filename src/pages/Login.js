461627e751250d16d64e159b4158d440718d1f8f
Rodrigo Silva Ferreira <rodrigosferreira0@gmail.com>
2023-06-05 17:02:09 -0300

    requisto 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className="login">
+      <h1>Login</h1>
+      <input type="email" data-testid="email-input" />
+      <input type="password" data-testid="password-input" />
+      <button data-testid="login-submit-btn">Entrar</button>
+    </div>
   );
 }
 
