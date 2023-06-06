eba4fdd32f0b70d9873b406b06876a2bbbb65c63
Fellipe Marcel <fmrclms@gmail.com>
2023-06-06 19:36:47 -0300

    group-14-req-26-e-27

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,23 +1,118 @@
-import React, { useEffect } from 'react';
-import { useHistory, useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import React, { useEffect, useState, useRef } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 
 function RecipeDetails() {
   const history = useHistory();
   const { id } = useParams();
-  useEffect(() => {
-    const url = history.location.pathname.includes('meals')
-      ? `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
-      : `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+  const [recommendation, setRecommendation] = useState(null);
+  const scrollContainerRef = useRef(null);
 
-    const getFoodInfo = async () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const isMealsPage = history.location.pathname.includes('meals');
+      const url = isMealsPage
+        ? `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+        : `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+
       const foodInfo = await fetch(url).then((response) => response.json());
       console.log(foodInfo);
+      const recommendationUrl = isMealsPage
+        ? 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
+        : 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+
+      const recommendationData = await fetch(recommendationUrl)
+        .then((response) => response.json());
+      setRecommendation(recommendationData);
     };
 
-    getFoodInfo();
-  }, [history.location.pathname]);
+    fetchData();
+  }, [history.location.pathname, id]);
 
-  return <div>oi</div>;
+  const scrollLeft = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollBy({
+        left: -scrollContainerRef.current.offsetWidth / 2,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  const scrollRight = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollBy({
+        left: scrollContainerRef.current.offsetWidth / 2,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  const magicNumberSix = 6;
+
+  return (
+    <div>
+      {recommendation && (
+        <div>
+          <h2>Recomendação:</h2>
+          <div
+            ref={ scrollContainerRef }
+            style={ {
+              display: 'flex',
+              overflowX: 'scroll',
+              width: '600px',
+            } }
+          >
+            {recommendation.drinks ? (
+              recommendation.drinks.slice(0, magicNumberSix).map((drink, index) => (
+                <div
+                  key={ index }
+                  data-testid={ `${index}-recommendation-card` }
+                  style={ {
+                    flex: '0 0 calc(600px / 2)',
+                    marginRight: '10px',
+                    border: '1px solid black',
+                    padding: '10px',
+                  } }
+                >
+                  <p data-testid={ `${index}-recommendation-title` }>
+                    {drink.strDrink}
+                  </p>
+                </div>
+              ))
+            ) : (
+              recommendation.meals.slice(0, magicNumberSix).map((meal, index) => (
+                <div
+                  key={ index }
+                  data-testid={ `${index}-recommendation-card` }
+                  style={ {
+                    flex: '0 0 calc(600px / 2)',
+                    marginRight: '10px',
+                    border: '1px solid black',
+                    padding: '10px',
+                  } }
+                >
+                  <p data-testid={ `${index}-recommendation-title` }>
+                    {meal.strMeal}
+                  </p>
+                </div>
+              ))
+            )}
+          </div>
+          {recommendation
+          && recommendation.drinks && recommendation.drinks.length > 2 && (
+            <button onClick={ scrollLeft }>
+              Scroll Left
+            </button>
+          )}
+          {recommendation
+          && recommendation.drinks && recommendation.drinks.length > 2 && (
+            <button onClick={ scrollRight }>
+              Scroll Right
+            </button>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default RecipeDetails;
