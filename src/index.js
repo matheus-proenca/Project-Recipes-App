20668c66dfd72785e00f66b5e896eab7ab747392
Livia Bastos <livialbastos@gmail.com>
2023-06-06 16:27:41 -0300

    req10 e req11

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Provider from './context/Provider';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+    <Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      ,
+    </Provider>,
   );
 
 // If you want your app to work offline and load faster, you can change
